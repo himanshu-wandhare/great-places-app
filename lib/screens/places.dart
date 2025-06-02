aa90bb8fa66ab933b1ff2ac8fcb70f2669c0522f
Himanshu Wandhare <himanshuwandhare13@gmail.com>
2025-06-03 00:42:45 +0530

    add sqlite database to store data locally on device

--- lib/screens/places.dart
+++ lib/screens/places.dart
@@ -6,11 +6,24 @@ import 'package:native_features/providers/places_provider.dart';
 import 'package:native_features/screens/add_place.dart';
 import 'package:native_features/widgets/places_list.dart';
 
-class Places extends ConsumerWidget {
+class Places extends ConsumerStatefulWidget {
   const Places({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<Places> createState() => _PlacesState();
+}
+
+class _PlacesState extends ConsumerState<Places> {
+  late Future<void> _placesFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _placesFuture = ref.read(placesProvider.notifier).loadPlaces();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final places = ref.watch(placesProvider);
 
     return Scaffold(
@@ -29,7 +42,14 @@ class Places extends ConsumerWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: PlacesList(places: places),
+        child: FutureBuilder(
+          future: _placesFuture,
+          builder:
+              (context, snapshot) =>
+                  snapshot.connectionState == ConnectionState.waiting
+                      ? const Center(child: CircularProgressIndicator())
+                      : PlacesList(places: places),
+        ),
       ),
     );
   }
